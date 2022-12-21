3789c648d68b725a183cd9e61b91a416b54687b5
DownedWings <102192700+DownedWings@users.noreply.github.com>
2022-12-20 21:40:40 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canavs = createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('myfilter.png');
+}
